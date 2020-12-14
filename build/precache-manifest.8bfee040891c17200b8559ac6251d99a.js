fb5085c2e6fca668813d9b1c52fbb2736d11c053
Ian Strand <strandoutmedia@gmail.com>
2020-12-14 12:36:17 -0800

    Attempt

---
+++ build/precache-manifest.8bfee040891c17200b8559ac6251d99a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6eec944e079882cd3d796dcc7049ff",
+    "url": "/ianstrand/webd171_react-youtube/index.html"
+  },
+  {
+    "revision": "d6ef28e9d1d8832cf292",
+    "url": "/ianstrand/webd171_react-youtube/static/css/main.1bd2a61a.chunk.css"
+  },
+  {
+    "revision": "6218d955b1b4887d0823",
+    "url": "/ianstrand/webd171_react-youtube/static/js/2.dc908593.chunk.js"
+  },
+  {
+    "revision": "0cc5f074416fc96c6ec20dfd8d9108c5",
+    "url": "/ianstrand/webd171_react-youtube/static/js/2.dc908593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fe863f6d517858bf70b",
+    "url": "/ianstrand/webd171_react-youtube/static/js/3.9e9a17ca.chunk.js"
+  },
+  {
+    "revision": "d6ef28e9d1d8832cf292",
+    "url": "/ianstrand/webd171_react-youtube/static/js/main.8b5564b0.chunk.js"
+  },
+  {
+    "revision": "66b4334adc3a247599a1",
+    "url": "/ianstrand/webd171_react-youtube/static/js/runtime-main.4d56fa5f.js"
+  },
+  {
+    "revision": "9cf01e1494f1fc137050c2d9b8797d07",
+    "url": "/ianstrand/webd171_react-youtube/static/media/Icon5.9cf01e14.png"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/ianstrand/webd171_react-youtube/static/media/brand-icons.13685372.ttf"
+  },
+  {
+    "revision": "3debcaf1a98e4445d213fd0089d42f6b",
+    "url": "/ianstrand/webd171_react-youtube/static/media/brand-icons.3debcaf1.svg"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/ianstrand/webd171_react-youtube/static/media/brand-icons.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/ianstrand/webd171_react-youtube/static/media/brand-icons.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/ianstrand/webd171_react-youtube/static/media/brand-icons.ec3cfdde.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/ianstrand/webd171_react-youtube/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/ianstrand/webd171_react-youtube/static/media/icons.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/ianstrand/webd171_react-youtube/static/media/icons.a0369ea5.eot"
+  },
+  {
+    "revision": "ad912fd102f4052d3273dd838e0c671d",
+    "url": "/ianstrand/webd171_react-youtube/static/media/icons.ad912fd1.svg"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/ianstrand/webd171_react-youtube/static/media/icons.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/ianstrand/webd171_react-youtube/static/media/icons.bea989e8.woff"
+  },
+  {
+    "revision": "fa6c55a6d47c5ae0c108d8e384b11d48",
+    "url": "/ianstrand/webd171_react-youtube/static/media/logo.fa6c55a6.png"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/ianstrand/webd171_react-youtube/static/media/outline-icons.261d666b.eot"
+  },
+  {
+    "revision": "89f9806b964f92ad282fd60947eba588",
+    "url": "/ianstrand/webd171_react-youtube/static/media/outline-icons.89f9806b.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/ianstrand/webd171_react-youtube/static/media/outline-icons.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/ianstrand/webd171_react-youtube/static/media/outline-icons.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/ianstrand/webd171_react-youtube/static/media/outline-icons.f89ea91e.woff"
+  }
+]);
